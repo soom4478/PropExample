b96b3d164001e2a7a66e9e68e23c63f7cf6ad1c0
soom <s2305@e-mirim.hs.kr>
2025-05-27 14:45:54 +0900

    고전적인 스타일로 Box 클래스 정의

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace PropExample
     {
         static void Main(string[] args)
         {
+            Box box1 = new Box(100, 200);
+            Console.WriteLine(box1.Area());
         }
     }
 }
