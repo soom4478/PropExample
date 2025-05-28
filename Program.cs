200ee23facb101cb0085225e43a3dd8128df5efa
soom <s2305@e-mirim.hs.kr>
2025-05-29 08:54:03 +0900

    클래스 변수 호출

--- Program.cs
+++ Program.cs
@@ -8,13 +8,23 @@ namespace PropExample
 {
     class Program
     {
+        public static int classVar = 0;
         static void Main(string[] args)
         {
+            // 클래스 변수 호출
+            Program.classVar = -1;
+
+            // 인스턴스 변수처럼 클래스 변수 사용 불가에 주의 (자바는 ok)
+            //Program p = new Program();
+
+            
+
+            // Property 실습
             Box box1 = new Box(100, 200);
             box1.Width = -3;
-            Console.WriteLine(box1.Width);
+            Console.WriteLine(box1.Width); 
 
-            Console.WriteLine(box1.Area());
+            Console.WriteLine(box1.Area);
         }
     }
 }
