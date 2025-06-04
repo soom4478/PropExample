546c5453431e5ba486aba12750a6d2526e7c7e6c
soom <s2305@e-mirim.hs.kr>
2025-06-04 13:55:16 +0900

    상수, readonly 키워드 비교 예시

---
+++ Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Item
+    {
+        private static int count;
+        public readonly int id;
+        public string name;
+        public int price;
+
+        public Item(string name, int price)
+        {
+            id = count++;
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -31,6 +31,18 @@ namespace PropExample
             Sample sample = new Sample();
             Console.WriteLine("세번째 위치");
 
+            // 상수, readonly 키워드 비교 예시
+            MyMath m = new MyMath();
+            Console.WriteLine(MyMath.PI);
+
+            Item item1 = new Item("고구마", 1000);
+            Item item2 = new Item("사과", 1500);
+            Item item3 = new Item("감자", 2000);
+            Console.WriteLine(item1.id);
+            Console.WriteLine(item2.id);
+            Console.WriteLine(item3.id);
+            // item3.id = 3; // readonly 키워드가 지정된 인스턴스 변수는 수정 불가
+
             // Property 실습
             Box box1 = new Box(100, 200);
             box1.Width = -3;
